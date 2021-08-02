45cdcf8e18904c33fe00cf89d6b0093f5aecccf3
Matt Munns <matthewmunns0@gmail.com>
2021-08-02 17:48:06 -0400

    logs

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,26 +7,6 @@ import Container from "react-bootstrap/Container";
 import Cookies from 'universal-cookie';
 import axios from 'axios';
 
-async function verifyToken(token){
-    console.log('trying to verify')
-    var data = {
-        action: 'verify_token',
-        token_to_verify: token
-        
-    }
-    const headers = {
-        'Content-Type': 'application/json',
-    }
-    const result = await axios.post(
-        'https://api.wordsoftheday.org/index-operation',
-        data, 
-        headers, 
-        {withCredentials:true}
-    )
-    const statusCode = result.status
-    return statusCode
-}
-
 export default class App extends Component {
     constructor() {
         super();
@@ -39,21 +19,29 @@ export default class App extends Component {
         this.handleLogout = this.handleLogout.bind(this)
     };
 
+    
 
     checkLoginStatus() {
         const login_cookie = new Cookies()
         console.log('checking login')
         if (login_cookie.get('auth_token')) {
             let token = login_cookie.get('auth_token')
-            const status_code = (async () => {const statusCode = await verifyToken(token); console.log(statusCode);
-                return statusCode})()
-            console.log(status_code)
-            if (status_code===200){
-                console.log('setting state')
-                this.setState({
-                    loggedIn:true
-                })
+            var data = {
+                action: 'verify_token',
+                token_to_verify: token
+                
             }
+            const headers = {
+                'Content-Type': 'application/json',
+            }
+            axios.post(
+                'https://api.wordsoftheday.org/index-operation',
+                data, 
+                headers, 
+                {withCredentials:true}
+            ).then(this.setState({
+                loggedIn:true
+            }))
             }
     };
 
